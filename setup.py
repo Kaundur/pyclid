48bf92e9be017d170ae1f63ed23ee45912d09934
Kaundur <gav.j.jones@googlemail.com>
2015-11-12 08:12:21 +0000

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='A simple vector mathematics module',
     author='Gavin Jones',
-    authore_email='gav.j.jones@googlemail.com',
+    author_email='gav.j.jones@googlemail.com',
     url='https://github.com/Kaundur/pyclid',
     download_url='https://github.com/Kaundur/pyclid/tarball/0.1',
     keywords=['Vector', 'Euclid'],
