aebc6de331e8291c55d661e871ffcf01bccf9da8
Kaundur <gav.j.jones@googlemail.com>
2015-11-12 14:01:01 +0000

    Added Vec3

--- pyclid/__init__.py
+++ pyclid/__init__.py
@@ -1,7 +1,5 @@
 import math
 
-# TODO - Should mag be a property of Vec2
-
 
 class Vec2:
     def __init__(self, x=0, y=0):
@@ -58,25 +56,61 @@ class Vec2:
         return Vec2((self.x + other.x)/2.0, (self.y + other.y)/2.0)
 
 
+class Vec3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
 
-# scalar triple product
-# vector triple product a.(bXc)
+    def __abs__(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
-# eq/ne
-# reflect
+    def __add__(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
 
-# function to find intercept between two vectors
+    def __sub__(self, other):
+        assert isinstance(other, Vec3), 'Requires a Vec3'
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
 
-# Matrix algebra
-# ident
+    def __mul__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x *= other
+        self.y *= other
+        self.z *= other
+        return self
 
-# Quaternion
-# raycast
-# surface
+    def __div__(self, other):
+        assert isinstance(other, (int, float, long)), 'Requires a int, float or long'
+        self.x /= other
+        self.y /= other
+        self.z /= other
+        return self
 
-# Make compatible with 2 and 3
-# Import numpy if available
+    def magnitude(self):
+        return self.__abs__()
 
+    def normalize(self):
+        mag = self.magnitude()
+        if mag:
+            self.x /= mag
+            self.y /= mag
+            self.z /= mag
+        return self
 
-# To work with other types
-# elif hasattr(other, '__len__') and len(other == 2):
+    # TODO - Requires more work, very different to cross of 2x2
+    # def cross(self, other):
+    #     assert isinstance(other, Vec3), 'Requires a Vec3'
+    #     return self.x*other.y - self.y*other.x
+
+    def dot(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec3'
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def angle(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec3'
+        return math.acos(self.dot(other)/(self.magnitude() * other.magnitude()))
+
+    def mid_point(self, other):
+        assert isinstance(other, Vec2), 'Requires a Vec3'
+        return Vec3((self.x + other.x)/2.0, (self.y + other.y)/2.0, (self.z + other.z)/2.0)
