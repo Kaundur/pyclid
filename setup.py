d4daf3198d3b211efbb6092bec9506c42a753564
Kaundur <gav.j.jones@googlemail.com>
2015-12-04 07:33:52 +0000

    Updated Vec2 rotation, still issues with compatibility of Mat4 with OpenGL matrices

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pyclid',
     packages=['pyclid'],
-    version='0.33',
+    version='0.34',
     description='A simple vector mathematics library',
     author='Gavin Jones',
     author_email='gav.j.jones@googlemail.com',
     url='https://github.com/Kaundur/pyclid',
-    download_url='https://github.com/Kaundur/pyclid/tarball/0.33',
+    download_url='https://github.com/Kaundur/pyclid/tarball/0.34',
     keywords=['Vector', 'Euclid'],
     classifiers=[],
 )
