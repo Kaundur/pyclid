d7fddde7858f19c9b4b5bd39e8d138695ef45c15
Kaundur <gav.j.jones@googlemail.com>
2015-12-17 08:13:41 +0000

    Updated release to version 0.40

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pyclid',
     packages=['pyclid'],
-    version='0.4',
+    version='0.40',
     description='A simple vector mathematics library',
     author='Gavin Jones',
     author_email='gav.j.jones@googlemail.com',
     url='https://github.com/Kaundur/pyclid',
-    download_url='https://github.com/Kaundur/pyclid/tarball/0.4',
+    download_url='https://github.com/Kaundur/pyclid/tarball/0.40',
     keywords=['Vector', 'Euclid', 'Matrix', 'Quaternion'],
     classifiers=[],
 )
